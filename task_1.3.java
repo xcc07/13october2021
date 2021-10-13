e1b65141de9cf6ab070e773f5c24dc0266c620b4
xcc07 <92458898+xcc07@users.noreply.github.com>
2021-10-13 19:37:23 +0300

    Create task_1.3.java

---
+++ task_1.3.java
@@ -0,0 +1,11 @@
+/* Объяви переменные a, b типа int и переменную s типа String.
+Сразу же в строке объявления присвой им любые разные значения. */
+
+public class Main {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int a = 1;
+        int b = 2;
+        String s = "aaaaa";
+    }
+}
