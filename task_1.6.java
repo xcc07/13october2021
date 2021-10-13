b900a2dbea742986c1dcbf97d96c025eb6a38daf
xcc07 <92458898+xcc07@users.noreply.github.com>
2021-10-13 20:22:23 +0300

    Update task_1.6.java

--- task_1.6.java
+++ task_1.6.java
@@ -2,8 +2,16 @@
 Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
 */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Ваш код
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("дай число => ");
+        double a = scan.nextInt();
+        System.out.println( a + " + 15% от " + a + " = " + a * 1.15 );
+        
     }
 }
